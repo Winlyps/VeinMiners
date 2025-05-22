2293beb6a14df23b2ecda93c8b14113dfe9b09ab
Winlypse <nts3@o2.pl>
2025-05-22 12:30:42 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/veinMiners/VeinMiners.kt
@@ -0,0 +1,14 @@
+package winlyps.veinMiners
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class VeinMiners : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
